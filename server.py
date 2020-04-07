f14cd7eb21ad0f887eca6a9df1d94f6d1f383888
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-04-07 08:27:31 -0700

    backend server

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+@app.route("/test")
+def test():
+    return "Hello test!"
+
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0')
---
+++ start.sh
@@ -0,0 +1,2 @@
+python3 -m pip install Flask
+python3 /380/server.py
