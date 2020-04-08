6ff25dacce59a3ea2b96588e353370f786f90052
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-04-08 14:11:45 -0700

    basic template

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return  render_template('index.html', title='Welcome')
+    return  render_template('index.jinja', title='Welcome')
 @app.route("/test")
 def test():
     return "Hello test!"
+
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
