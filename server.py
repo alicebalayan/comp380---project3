fa03b88a8e7c0a5e0c2851e328a54909faaca03e
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2020-04-08 18:33:26 -0700

    add session

--- server.py
+++ server.py
@@ -1,24 +1,44 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import Flask, redirect, url_for, request
-
+from flask import Flask, session, redirect, url_for, escape, request
+import random
+import string
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 app = Flask(__name__)
-
+app.secret_key =  randomString()
 @app.route("/")
 def index():
+    if 'username' in session:
+        return redirect("/dashboard")
     return  render_template('index.jinja', title='Please login')
 @app.route('/login',methods = ['POST','GET'])
 def login():
     if request.method== "GET":
         return redirect("/")
-    user = request.form.get('username')
-    password= request.form.get('pass')
+    user = request.form['username']
+    password= request.form['pass']
     print(user)
     print(password)
     if user=="user" and password =="password":
-        return render_template('login.jinja')
+        session['username'] = user
+        return redirect("/dashboard")
     return render_template('test.html')
+@app.route('/dashboard')
+def dashboard(): 
+    if not 'username' in session and session['username'] !="user":
+        return redirect("/logout")
+    return  render_template('index.jinja', title='hello '+ session['username'], loggedin=True)
+
+@app.route('/logout')
+def logout(): 
+    if 'username' in session:
+        # remove the username from the session if it is there
+        session.pop('username', None)
+    return redirect("/")
 
 
 if __name__ == "__main__":
