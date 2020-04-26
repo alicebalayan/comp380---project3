94bf5c036ca189e49f1fab6ceff75a765fb06b7a
Arian-D <ArianDehghani@protonmail.com>
2020-04-25 19:01:46 -0700

    shortened and changed the field names to camel_case

--- setup.sql
+++ setup.sql
@@ -2,78 +2,125 @@
 -- NOT APPROVED BY ALICE, YET!!!
 
 -- Table for deliverables
-CREATE TABLE IF NOT EXISTS Deliverables (
-    uniqueIdDeliverable INT NOT NULL AUTO_INCREMENT,
-    nameDeliverable TEXT,
-    descriptionDeliverable LONGTEXT,
-    dueDateDeliverable DATE,
-    PRIMARY KEY (uniqueIdDeliverable)
+-- No dependencies
+CREATE TABLE IF NOT EXISTS deliverables (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    description LONGTEXT,
+    due_date DATE,
+    PRIMARY KEY (id)
+);
+
+-- Table for requirements
+-- Depends on deliverables
+CREATE TABLE IF NOT EXISTS requirements (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    deliverable_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (deliverable_id)
+      REFERENCES deliverables(id)
 );
 
 -- Table for tasks
-CREATE TABLE IF NOT EXISTS Tasks (
-    uniqueIdTask INT NOT NULL AUTO_INCREMENT,
-    nameTask TEXT,
-    descriptionTask LONGTEXT,
-    dueDateDeliverable DATE,
-    expectedStartDateTask DATE,
-    expectedEndDateTask DATE,
-    expectedDurationTask DECIMAL,
-    expectedEffortTask DECIMAL,
-    actualStartDateTask DATE,
-    actualEndDateTask DATE,
-    actualDurationTask DECIMAL,
-    effortCompletionTask DECIMAL,
-    actualEffortTask DECIMAL,
-    percentCompletedTask DECIMAL,
-    uniqueIdDeliverable INT NOT NULL
-    PRIMARY KEY (uniqueIdTask),
-    FOREIGN KEY (uniqueIdDeliverable)
-      REFERENCES Deliverables(uniqueIdDeliverable)
+-- Depends on deliverables
+CREATE TABLE IF NOT EXISTS tasks (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    description LONGTEXT,
+    due_date DATE,
+    expected_start_date DATE,
+    expected_end_date DATE,
+    expected_duration DECIMAL,
+    expected_effort DECIMAL,
+    actual_start_date DATE,
+    actual_end_date DATE,
+    actual_duration DECIMAL,
+    effort_completed DECIMAL,
+    actual_effort DECIMAL,
+    percent_complete DECIMAL,
+    deliverable_id INT NOT NULL
+    PRIMARY KEY (id),
+    FOREIGN KEY (deliverable_id)
+      REFERENCES deliverables(id)
 );
 
 -- Table for issues
-CREATE TABLE IF NOT EXISTS Issues (
-    uniqueIdIssue INT NOT NULL AUTO_INCREMENT,
-    nameIssue TEXT,
-    descriptionIssue LONGTEXT,
-    priorityIssue TEXT,
-    severityIssue TEXT,
-    dateRaisedIssue DATE,
-    dateAssignedIssue DATE,
-    expectedCompletionDateIssue DATE,
-    actualCompletionDateIssue DATE,
-    statusIssue TEXT,
-    statusDescriptionIssue LONGTEXT,
-    updateDateIssue DATE,
-    PRIMARY KEY (uniqueIdIssue)
+-- No dependencies
+CREATE TABLE IF NOT EXISTS issues (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    description LONGTEXT,
+    priority TEXT,
+    severity TEXT,
+    date_raised DATE,
+    date_assigned DATE,
+    expected_completion_date DATE,
+    actual_completion_date DATE,
+    status TEXT,
+    status_description LONGTEXT,
+    update_date DATE,
+    PRIMARY KEY (id)
+);
+
+-- Table for the task-issue relation
+-- Depends on issues and tasks
+CREATE TABLE task_issue (
+    issue_id INT NOT NULL,
+    task_id INT NOT NULL,
+    FOREIGN KEY (issue_id)
+      REFERENCES issues(id),
+    FOREIGN KEY (task_id)
+      REFERENCES tasks(id),
 );
 
 -- Table for action items
-CREATE TABLE ActionItems (
-    uniqueIdActionItem INT NOT NULL AUTO_INCREMENT,
-    nameActionItem TEXT,
-    descriptionActionItem LONGTEXT,
-    dateCreatedActionItem DATE,
-    dateAssignedActionItem DATE,
-    resourceAssignedActionItem DATE,
-    expectedCompletionDateActionItem DATE,
-    actualCompletionDateActionItem DATE,
-    statusActionItem TEXT,
-    statusDescriptionActionItem TEXT,
-    updateDateActionItem DATE,
-    uniqueIdIssue INT NOT NULL,
-    PRIMARY KEY (uniqueIdActionItem),
-    FOREIGN KEY (uniqueIdIssue)
-      REFERENCES Issues(uniqueIdIssue)
+-- Depends on issues
+CREATE TABLE action_items (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    description LONGTEXT,
+    date_created DATE,
+    date_assigned DATE,
+    resource_assigned DATE,
+    expected_completion_date DATE,
+    actual_completion_date DATE,
+    status TEXT,
+    status_description TEXT,
+    update_date DATE,
+    issue_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (issue_id)
+      REFERENCES issues(id)
+);
+
+-- Table for decisions
+-- No dependencies
+CREATE TABLE decisions (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    description LONGTEXT,
+    priority TEXT,
+    impact TEXT,
+    date_created DATE,
+    date_needed DATE,
+    date_made DATE,
+    expected_completion_date DATE,
+    actual_completion_date DATE,
+    note_date DATE,
+    status TEXT,
+    status_description LONGTEXT,
+    update_date DATE,
+    PRIMARY KEY (id)
 );
 
 -- Table for resources 
-CREATE TABLE Resources (
-    uniqueIdResource INT NOT NULL AUTO_INCREMENT,
-    nameResource TEXT,
-    uniqueIdActionItem INT NOT NULL,
-    PRIMARY KEY (uniqueIdResource),
-    FOREIGN KEY (uniqueIdActionItem)
-      REFERENCES ActionItems(uniqueIdActionItem)
-); 
+-- Depends on action_items
+CREATE TABLE resources (
+    id INT NOT NULL AUTO_INCREMENT,
+    name TEXT,
+    action_items_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (action_items_id)
+      REFERENCES action_items(id)
+);
