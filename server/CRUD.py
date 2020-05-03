d2abfba3546e782ff9a832461484bfa877fe12e1
Arian-D <ArianDehghani@protonmail.com>
2020-05-03 13:47:40 -0700

    Added retreive and delete methods for CRUD

--- server/CRUD.py
+++ server/CRUD.py
@@ -8,6 +8,13 @@ class CRUD(dict):
         self.table = table
         self.unique_id = None
 
+    def _type_check(value):
+        # Type check for SQL queries
+        if type(value) == str:
+            return f'"{value}"'
+        # TODO: check datetime type
+        return str(value)
+    
     def create(self) -> None:
         # Insert a populated class in the corresponding table
         connection = connect()
@@ -17,25 +24,45 @@ class CRUD(dict):
                     f"INSERT INTO {self.table} (" +
                     (', '.join(f"{key}" for key in self.keys())) +
                     ") VALUES(" +
-                    (', '.join(f'"{value}"' for value in self.values())) +
+                    (', '.join(self._type_check(value) for value in self.values())) +
                     ")"
                 )
                 print(query)
                 cursor.execute(query)
             connection.commit()
-        except Error as e:
-            print(e)            # TODO: Add proper logging
         finally:
             connection.close()
             
-    def retreive(self, unique_id: str) -> None: # TODO
-        # Populate the class using a preexisting record
-        pass
+    def retreive(self, unique_id: int) -> None:
+        # Populate the class preexsiting values
+        connection = connect()
+        try:
+            with connection.cursor() as cursor:
+                query = f"SELECT * FROM {self.table} WHERE id={unique_id}"
+                print(query)
+                cursor.execute(query)
+                result = cursor.fetchone()
+                if result is not None:
+                    self.update(result)
+        finally:
+            self.unique_id = unique_id
+            connection.close()
 
     def update_remote(self) -> None:
         # Update the DB record based on the local fields
         pass
         
-    def delete(self) -> None:   # TODO
-        # Delete the remote record
-        pass
+    def delete(self) -> None:
+        # Delete the remote and local record
+        if self.unique_id is None:
+            return
+        connection = connect()
+        try:
+            with connection.cursor() as cursor:
+                query = f"DELETE FROM {self.table} WHERE id={self.unique_id}"
+                print(query)
+                cursor.execute(query)
+            connection.commit()
+        finally:
+            self.clear()
+            connection.close()
--- server/server.py
+++ server/server.py
@@ -24,11 +24,11 @@ app.secret_key = urandom(16)
 
 def connect():
     # Connection instance to be used with other functions
-    return pymysql.connect(host=app.config['DATABASE_SERVER'],
-                           port=app.config['DATABASE_PORT'],
-                           user=app.config['DATABASE_USER'],
-                           password=app.config['DATABASE_PASSWORD'],
-                           db=app.config['DATABASE_DB'],
+    return pymysql.connect(host=AppConfig.DATABASE_SERVER()
+                           port=AppConfig.DATABASE_PORT()
+                           user=AppConfig.DATABASE_USER
+                           password=AppConfig.DATABASE_PASSWORD
+                           db=DATABASE_DB
                            charset='utf8mb4',
                            cursorclass=pymysql.cursors.DictCursor)
 
